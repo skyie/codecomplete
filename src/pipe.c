28d14497d199bc9ec6914714a0c451379e917cfa
skyie <503740202@qq.com>
2017-02-06 18:11:17 +0800

    Add files via upload
    add files

---
+++ src/pipe.c
@@ -0,0 +1,42 @@
+/**
+ * 线程间使用管道通信
+ */
+
+#include <stdio.h>  // printf
+#include <stdlib.h> // exit
+#include <unistd.h> // pipe
+#include <string.h> // strlen
+#include <pthread.h> // pthread_create
+
+
+void *func(void* fd)
+{
+        printf("write fd = %d\n", *(int*)fd);
+        char str[] = "hello everyone!";
+        write(*(int*)fd, str, strlen(str));
+}
+
+int main()
+{
+    int fd[2];
+    char readbuf[1024];
+
+    if(pipe(fd) < 0)
+    {
+        printf("pipe error!\n");
+    }
+
+    // create a new thread
+    pthread_t tid = 0;
+    pthread_create(&tid, NULL, func, &fd[1]);
+    pthread_join(tid, NULL);
+
+    sleep(3);
+
+    // read buf from child thread
+    read(fd[0], readbuf, sizeof(readbuf));
+    printf("read buf = %s\n", readbuf);
+
+    return 0;
+}
+
