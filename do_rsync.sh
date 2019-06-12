3e7ec2cc7514d06ec6e61b3883563c40414944db
skyie <503740202cy@gmail.com>
2019-06-12 22:21:37 +0800

    commit

--- do_rsync.sh
+++ do_rsync.sh
@@ -4,4 +4,4 @@ set -ex
 
 #rsync -avz --password-file=../rsyncd.secrets  --include-from="include_file" --exclude="./*" ../codecomplete   root@192.168.199.135::share
 
-rsync -avz --password-file=../rsyncd.secrets  --include="src/c" --exclude="./" ../codecomplete   root@192.168.199.135::share
+rsync -avz --password-file=../rsyncd.secrets   ../codecomplete   root@192.168.199.135::share
