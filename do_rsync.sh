cb391f3d203164064064feee70c709a324ddc47f
brayden <brayden8@163.com>
2020-10-20 23:05:34 +0800

    commit

--- do_rsync.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-#rsync -avz --password-file=../rsyncd.secrets  --include-from="include_file" --exclude="./*" ../codecomplete   root@192.168.199.135::share
-
-rsync -avz --password-file=../rsyncd.secrets   ../codecomplete   root@192.168.199.135::share
